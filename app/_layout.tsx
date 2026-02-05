0ebc8c122ad917b17f196e0f58e1a4864ee4aa01
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2026-02-05 22:29:42 +0530

    load new fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Poppins: require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
+
+// import { Stack } from "expo-router";
+
+// export default function RootLayout() {
+//   return <Stack screenOptions={{ headerShown: false }} />;
+// }
